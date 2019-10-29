17e1899d02cf599898c02182c757965154f72e51
HaiNamNguyen3012 <47827233+HaiNamNguyen3012@users.noreply.github.com>
2019-10-29 09:12:11 +0700

    18020043

--- src/code/GameEnity/Enemy/NormalEnemy.java
+++ src/code/GameEnity/Enemy/NormalEnemy.java
@@ -1,4 +1,23 @@
 package code.GameEnity.Enemy;
 
-public class NormalEnemy {
+import javax.swing.*;
+import java.awt.*;
+import static code.Config.*;
+public class NormalEnemy extends Rectangle implements Enemy {
+    private int armor = 50;
+    private int speedRunning = 50;
+    private int blood = 50;
+    public NormalEnemy(int x , int y, int width, int height, int armor, int speedRunning, int blood){
+        this.armor = armor;
+        this.blood = blood;
+        this.speedRunning = speedRunning;
+        for(int i=0; i<field_height; i++){
+            for(int j=0; j<field_width; j++){
+                if(map[i][j]==1){
+                    setBounds(x, y, width, height);
+                }
+            }
+        }
+    }
+    public void draw (Graphics g){g.drawImage(img_Enemy[0],x,y,width,height,null);}
 }
