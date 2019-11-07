7d601c048807082953f81a67bd728f2614cd7258
HaiNamNguyen3012 <47827233+HaiNamNguyen3012@users.noreply.github.com>
2019-11-07 23:17:55 +0700

    18020043

--- src/code/GameEnity/Enemy/TankerEnemy.java
+++ src/code/GameEnity/Enemy/TankerEnemy.java
@@ -1,4 +1,89 @@
 package code.GameEnity.Enemy;
 
-public class TankerEnemy {
+import java.awt.*;
+import java.util.Random;
+
+import static code.Config.*;
+
+public class TankerEnemy extends Rectangle implements Enemy {
+    private int blood_ = BLOOD[TANKER_ENEYMY];
+    private final int MAX_BLOOD_ = blood_;
+    private final int ARMOR_ = ARMOR[TANKER_ENEYMY];
+    private final double SPEED_ = 0.1;
+    private final int REWARD_ = REWARD[TANKER_ENEYMY];
+    private double x_;
+    private double y_;
+    private double dx_;
+    private double dy_;
+    private Point p_next;
+    private Random rd = new Random();
+    private int points_appear = rd.nextInt(spawners.size());
+
+    public TankerEnemy() {
+        setBounds(spawners.get(points_appear).x + (TILE_SIZE - ENEMIES_SIZE[TANKER_ENEYMY]) / 2, spawners.get(points_appear).y + (TILE_SIZE - ENEMIES_SIZE[TANKER_ENEYMY]) / 2, ENEMIES_SIZE[TANKER_ENEYMY], ENEMIES_SIZE[TANKER_ENEYMY]);
+        x_ = x;
+        y_ = y;
+        p_next = new Point(spawners.get(points_appear).x + TILE_SIZE / 2, spawners.get(points_appear).y + TILE_SIZE / 2);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.red);
+        g.drawRect(x, y - 6, width*blood_/MAX_BLOOD_, 3);
+        g.fillRect(x, y - 6, width*blood_/MAX_BLOOD_, 3);
+    /*    g.drawImage(img_Enemies[NORMAL_ENEMY], x, y, width, height, null);
+        if (!status_clicks[BUTTON_PAUSE]) {
+            move();
+        }
+
+     */
+        g.setColor(Color.orange);
+        g.fillRect(x,y,width,height);
+        move();
+    }
+
+    public void move() {
+        if (p_next.x == x + width / 2 && p_next.y == y + height / 2) {
+            determine_p_next();
+        }
+        x_ += dx_;
+        y_ += dy_;
+        x = (int) x_;
+        y = (int) y_;
+    }
+
+    public void determine_p_next() {
+        dx_ = 0;
+        dy_ = 0;
+        int i = (p_next.y - SPACE_ABOVE - TILE_SIZE / 2) / TILE_SIZE;
+        int j = (p_next.x - TILE_SIZE / 2) / TILE_SIZE;
+        if (i > 0 && map[i][j] > map[i - 1][j] && map[i - 1][j] != -1) {
+            --i;
+            dy_ = -SPEED_;
+        } else if (i < FIELD_ROWS - 1 && map[i][j] > map[i + 1][j] && map[i + 1][j] != -1) {
+            ++i;
+            dy_ = SPEED_;
+        } else if (j > 0 && map[i][j] > map[i][j - 1] && map[i][j - 1] != -1) {
+            --j;
+            dx_ = -SPEED_;
+        } else if (j < FIELD_COLUMNS - 1 && map[i][j] > map[i][j + 1] && map[i][j + 1] != -1) {
+            ++j;
+            dx_ = SPEED_;
+        }
+        p_next.x = j * TILE_SIZE + TILE_SIZE / 2;
+        p_next.y = i * TILE_SIZE + TILE_SIZE / 2 + SPACE_ABOVE;
+    }
+
+    public void subtractBlood_(int damage) {
+        if (damage > ARMOR_) {
+            this.blood_ -= (damage - ARMOR_);
+        }
+    }
+
+    public int getBlood_() {
+        return blood_;
+    }
+
+    public int getREWARD_() {
+        return REWARD_;
+    }
 }
